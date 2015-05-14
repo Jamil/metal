1de3e02fe16a99b77489d45c251b8e8ee44b74c0
Jamil Dhanani <jamil.dhanani@gmail.com>
2015-05-14 19:54:34 -0400

    Add flightplan (deploy) script

---
+++ flightplan.js
@@ -0,0 +1,28 @@
+// flightplan.js
+var plan = require('flightplan');
+
+// configuration
+plan.target('production', [
+    {
+        host: 'crosswind.io',
+        username: 'ubuntu',
+        privateKey: '/Users/jamildhanani/.ssh/crosswind.pem'
+    }
+    ]
+);
+
+plan.remote(function(remote) {
+    remote.with('cd /metal/', function() {
+        remote.log('Pull latest code from git');
+        remote.git('pull');
+
+        remote.log('Install dependencies');
+        remote.exec('npm install');
+
+        remote.log('Install default data in Mongo')
+        remote.exec('find ./models -name "*_defaults.js" | xargs mongo')
+
+        remote.log('Restart application');
+        remote.exec('pm2 restart app.js')
+    });
+});
