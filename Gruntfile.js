3ab7b008c189ebde00d0a2ab684096051bceb0c9
Jamil Dhanani <jamil.dhanani@gmail.com>
2015-05-14 20:43:58 -0400

    Add Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+    require('load-grunt-tasks')(grunt);
+    grunt.initConfig({
+        jshint: {
+            files: ['Gruntfile.js',
+                    'flightplan.js',
+                    'app.js',
+                    'models/*.js'],
+            options: {
+                globals: {
+                    jQuery: false
+                }
+            }
+        },
+        shell: {
+            options: {
+                stderr: false
+            },
+            syncdb: {
+                command: 'echo "Install default data in Mongo" ; find ./models -name "*_defaults.js" | xargs mongo'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.registerTask('default', ['jshint', 'shell']);
+};
