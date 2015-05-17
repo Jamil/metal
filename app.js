8697b8a76def9cedde61b780e664aeb94274a2d1
Jamil Dhanani <jamil.dhanani@gmail.com>
2015-05-17 19:30:35 -0400

    Remove all routes from app.js

--- app.js
+++ app.js
@@ -23,13 +23,6 @@ app.use(passport.session());
 
 app.use(express.static(path.join(application_root, "public")));
 
-// Schema
-var StatusProgramModel = mongoose.model('StatusProgram');
-
-app.get('/', function(req, res){
-    res.render('index');
-});
-
 // Server
 var use_port;
 
@@ -47,7 +40,6 @@ var server = app.listen(use_port, function () {
 
   var host = server.address().address;
   var port = server.address().port;
-
   console.log('Example app listening at http://%s:%s', host, port);
 
 });
