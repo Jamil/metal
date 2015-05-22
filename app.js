d74aa3da7a167bdba3bbe4f41f6b5f7c454f3832
Jamil Dhanani <jamil.dhanani@gmail.com>
2015-05-21 21:14:39 -0400

    Add DL fare classes

--- app.js
+++ app.js
@@ -6,17 +6,29 @@ var app = express();
 var path = require('path');
 var fs = require('fs');
 var mongoose = require('mongoose');
-var cookieParser = require('cookie-parser');
-var flash = require('connect-flash');
 var passport = require('passport');
-var expressSession = require('express-session');
+
+var flash = require('connect-flash');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var models = require('./models');
-var routes = require('./routes')(app);
+var routes = require('./routes')(app, passport);
 
 // Configuring Express
-app.engine('.html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({ secret: 'sessionsecret' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
 app.use(express.static(path.join(application_root, "public")));
 
