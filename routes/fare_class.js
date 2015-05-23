63a844fa178a0451f5240efda359a5d760f3b1fa
Jamil Dhanani <jamil.dhanani@gmail.com>
2015-05-23 17:10:33 -0400

    Add endpoints for crediting to program

--- routes/fare_class.js
+++ routes/fare_class.js
@@ -37,13 +37,42 @@ module.exports = function(app){
     });
 
     app.get('/api/fare_classes/:airline/:id', function(req, res) {
-        FareClassModel.find({
+        FareClassModel.findOne({
             "airline": req.params.airline,
             "fare": req.params.id
         }, function(err, obj) {
             if (err)
                 res.send(err);
             res.json(obj);
+        });
+    });
+
+    app.get('/api/fare_classes/:airline/:id/credit/', function(req, res) {
+        FareClassModel.findOne({
+            "airline": req.params.airline,
+            "fare": req.params.id
+        }, function(err, obj) {
+            if (err)
+                res.send(err);
+            res.json(obj.credit);
+        });
+    });
+
+    app.get('/api/fare_classes/:airline/:id/credit/:program/', function(req, res) {
+        FareClassModel.findOne({
+            "airline": req.params.airline,
+            "fare": req.params.id
+        }, function(err, obj) {
+            if (err)
+                res.send(err);
+            for (var i = 0; i < obj.credit.length; i++) {
+                if (obj.credit[i].status_program == req.params.program) {
+                    res.json(obj.credit[i]);
+                }
+            }
+            res.status(404).json({
+                "Not Found": ("The fare class " + req.params.id + " on " + req.params.airline + " cannot be credited to " + req.params.program)
+            });
         });
     });
 };
