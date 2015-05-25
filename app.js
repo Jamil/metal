eb5ced960f6e774ae62711e18463179e0dc347ba
Jamil Dhanani <jamil.dhanani@gmail.com>
2015-05-25 14:31:06 -0400

    Add passport views, config, connect-flash setup

--- app.js
+++ app.js
@@ -5,32 +5,34 @@ var app = express();
 
 var path = require('path');
 var fs = require('fs');
-var mongoose = require('mongoose');
-var passport = require('passport');
-
 var flash = require('connect-flash');
+var mongoose = require('mongoose');
 var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var passport = require('passport');
 
 var models = require('./models');
-var routes = require('./routes')(app, passport);
 
 // Configuring Express
-app.use(morgan('dev')); // log every request to the console
-app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser()); // get information from html forms
-
 app.set('view engine', 'ejs'); // set up ejs for templating
+
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser('secret')); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
 
 // required for passport
 app.use(session({ secret: 'sessionsecret' })); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
+require('./config/passport')(passport);
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
 app.use(express.static(path.join(application_root, "public")));
+
+var routes = require('./routes')(app, passport);
 
 // Server
 var use_port;
--- models/user.js
+++ models/user.js
@@ -1,8 +1,23 @@
 var mongoose = require('mongoose');
 
-module.exports = mongoose.model('User', {
-    _id: String,
-    username: String,
-    password: String,
-    email: String
+var userSchema = mongoose.Schema({
+    local: {
+        email: String,
+        password: String
+    }
 });
+
+
+// methods ======================
+// generating a hash
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('User', userSchema);
